bfc3fd4157bf28fe20c25079876a795bdd52fa70
shanni kumar <shanni.kumar@brihaspatitech.com>
2018-10-09 14:20:07 +0530

    Fixing open/close price issues

--- api/services/MobileApisService.js
+++ api/services/MobileApisService.js
@@ -658,8 +658,8 @@ module.exports = {
 									price=_.head(price);
 									price.data_date=moment(totalCryptoPrice.data_date).format('YYYY-MM-DD');
 									if(!price.hasOwnProperty('open') && (price.high) && (price.low)){
-										price.open=(Math.random() * price.high) + price.low;  
-										price.close=(Math.random() * price.high) + price.low;  
+										price.open=(Math.random() * (price.high - price.low) + price.low);  
+										price.close=(Math.random() * (price.high - price.low) + price.low);  
 									}
 									return_array.push(price);
 								}
@@ -696,8 +696,8 @@ module.exports = {
 									price=_.head(price);
 									price.data_date=moment(totalCryptoPrice.data_date).format('YYYY-MM-DD');
 									if(!price.hasOwnProperty('open') && (price.high) && (price.low)){
-										price.open=(Math.random() * price.high) + price.low;  
-										price.close=(Math.random() * price.high) + price.low;  
+										price.open=(Math.random() * (price.high - price.low) + price.low);  
+										price.close=(Math.random() * (price.high - price.low) + price.low);   
 									}
 									return_array.push(price);
 								}
